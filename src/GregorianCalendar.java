39d0b9dd81d9a0c93969eecfa226f1202b55ea82
Shunsuke Shida <shunsuke.sida@gmail.com>
2011-12-29 19:20:02 +0900

    remove October1582

--- src/GregorianCalendar.java
+++ src/GregorianCalendar.java
@@ -1,14 +1,14 @@
 
 public class GregorianCalendar extends Calendar {
-	@Override static int correctedByCalendar = 2;
+	static int correctedByCalendar = 2;
 	
-	@Override static int correctedByYear(int year, int parameter) {
+	static int correctedByYear(int year, int parameter) {
 		//year -= year / 10000 * 10000;
 		year -= parameter;
 		return year + year / 4 - year / 100 + year / 400 + parameter;
 	}
 	
-	@Override static boolean isLeapYearOftheCommonEra(int year) {
+	static boolean isLeapYearOftheCommonEra(int year) {
 		return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) ? true : false;
 	}
 }
--- src/JulianCalendar.java
+++ src/JulianCalendar.java
@@ -1,14 +1,14 @@
 
 public class JulianCalendar extends Calendar {	
-	@Override static int correctedByCalendar = 4;
+	static int correctedByCalendar = 4;
 	
-	@Override static int correctedByYear(int year, int parameter) {
+	static int correctedByYear(int year, int parameter) {
 		//year -= year / 10000 * 10000;
 		year -= parameter;
 		return year + year / 4 + parameter;
 	}
 	
-	@Override static boolean isLeapYearOftheCommonEra(int year) {
+	static boolean isLeapYearOftheCommonEra(int year) {
 		return year % 4 == 0 ? true : false;
 	}
 }
